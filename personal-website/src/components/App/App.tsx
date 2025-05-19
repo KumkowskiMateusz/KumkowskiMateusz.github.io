f9d50812a1c2c73b0f71badc6ed5f87f5114bd48
mateusz kumkowski <kumkowskimateusz2002@gmail.com>
2025-05-19 14:23:52 -0400

    feat: Update React and React-DOM to version 19.1.0, add AboutMe component with styling

--- personal-website/src/components/App/App.tsx
+++ personal-website/src/components/App/App.tsx
@@ -3,11 +3,9 @@ import BubbleBackground from '../bubbleBackground/BubbleBackground'
 import IntroCard from '../IntroCard/IntroCard'
 import { useState } from 'react';
 import ProjectGrid from '../ProjectGrid/ProjectGrid';
-import useSnapping from '../../customHooks/useSnapping';
-import useWindowDimensions from '../../customHooks/useWindowDimension';
 import Navbar from '../Navbar/Navbar';
-import TransitionGradient from '../TransitionGradient/TransitionGradient';
 import TransitionLine from '../TransitionLine/TransitionLine';
+import AboutMe from './AboutMe/AboutMe';
 
 function App() {
 
@@ -55,11 +53,7 @@ function App() {
         </div>
         <TransitionLine size='70%' thickness='5px' color='white' marginLeft='auto' marginRight='0px' marginBlock='2rem'/>
         <div id='about-me' className='about-me'>
-          <h1>About Me</h1>
-          <p>As a passionate and driven Computer Science enthusiast, I am deeply committed to expanding my technical expertise and exploring innovative solutions to real-world problems. I’ve had hands-on experience developing full-stack applications using Spring Boot, React, and AWS, while also applying modern tools like Docker, Jenkins, and JUnit to create scalable, secure, and efficient systems. I thrive in collaborative, Agile environments and have taken on leadership roles such as Scrum Master to drive team productivity and ensure timely delivery.</p>
-          <p>Currently, I am eager to further my career by seeking an internship/associate position within New York, where I can leverage my skills in software development and continue to grow in areas like cloud architecture, DevOps, and data analytics. My focus is to gain practical experience, contribute to impactful projects, and further develop my problem-solving abilities.</p>
-          <p>I’m excited to learn from professionals in the field and bring my enthusiasm for technology and continuous improvement to new challenges.</p>
-          <p>Image slideshow w/ info</p>
+          <AboutMe/>
         </div>
         <TransitionLine size='70%' thickness='5px' color='white' marginLeft='0px' marginRight='0px' marginBlock='2rem'/>
 
